a4c5bb5a4b2f2605ef7e33db26921f06ad926fc8
David Amanshia <dave@pharoah-tools.org.uk>
2014-09-05 14:15:43 +0100

    cap workstartion bits

---
+++ build/config/cleopatra/cleofy/autopilots/custom/Phlagrant/cleofy-cm-cap-workstation.php
@@ -0,0 +1,38 @@
+<?php
+
+Namespace Core ;
+
+class AutoPilotConfigured extends AutoPilot {
+
+    public $steps ;
+    protected $myUser ;
+
+    public function __construct() {
+        $this->setSteps();
+    }
+
+    /* Steps */
+    protected function setSteps() {
+
+        $this->steps =
+            array(
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets install Pharaoh Tools for Capgemini"),),),
+
+                // All Pharoes
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets ensure Cleopatra" ),),),
+                array ( "Cleopatra" => array( "ensure" => array(),),),
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets ensure Dapperstrano" ),),),
+                array ( "Dapperstrano" => array( "ensure" => array(),),),
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets ensure Testingkamen" ),),),
+                array ( "Testingkamen" => array( "ensure" => array(),),),
+
+                array ( "Logging" => array( "log" => array(
+                    "log-message" => "Capgemmini worksaion done"
+                ),),),
+
+            );
+
+    }
+
+
+}
