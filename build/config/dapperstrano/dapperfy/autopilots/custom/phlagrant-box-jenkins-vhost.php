e13a2ea5b3a4eea4e6b8bfc51d0c188de2270523
David Amanshia <dave@pharoah-tools.org.uk>
2014-09-05 12:47:30 +0100

    hack vhost template to reverse proxy. there is a reverse proxy command so this isnt the best way

--- build/config/dapperstrano/dapperfy/autopilots/custom/phlagrant-box-jenkins-vhost.php
+++ build/config/dapperstrano/dapperfy/autopilots/custom/phlagrant-box-jenkins-vhost.php
@@ -29,7 +29,7 @@ class AutoPilotConfigured extends AutoPilot {
               array ( "Logging" => array( "log" => array( "log-message" => "Next create our host file entry for our local URL"), ) ),
               array ( "HostEditor" => array( "add" => array (
                   "guess" => true,
-                  "host-name" => "www.drupal-hello.vm",
+                  "host-name" => "www.drupal-jenkins.vm",
               ), ), ),
 
               array ( "Logging" => array( "log" => array( "log-message" => "Next create our virtual host"), ) ),
@@ -42,49 +42,7 @@ class AutoPilotConfigured extends AutoPilot {
                   "vhe-template" => $this->getTemplate(),
               ), ), ),
 
-              array ( "Logging" => array( "log" => array( "log-message" => "Next ensure our db file configuration is reset to blank" ), ), ),
-              array ( "DBConfigure" => array( "drupal7-reset" => array(
-                  "parent-path" => "/var/www/drupal-hello/",
-                  "platform" => "drupal7",
-              ), ), ),
-
-              array ( "Logging" => array( "log" => array("log-message" => "Next configure our projects db configuration file"), ) ),
-              array ( "DBConfigure" => array( "drupal7-conf" => array(
-                  "parent-path" => "/var/www/drupal-hello/",
-                  "mysql-host" => "127.0.0.1",
-                  "mysql-user" => "ph_user",
-                  "mysql-pass" => "ph_pass",
-                  "mysql-db" => "ph_db",
-                  "mysql-platform" => "drupal7",
-                  "mysql-admin-user" => "root",
-                  "mysql-admin-pass" => "cleopatra",
-              ), ) , ) ,
-
-              array ( "Logging" => array( "log" => array( "log-message" => "Now lets drop our current database if it exists"), ) ),
-              array ( "DBInstall" => array( "drop" => array(
-                  "parent-path" => "/var/www/drupal-hello/",
-                  "mysql-host" => "127.0.0.1",
-                  "mysql-user" => "ph_user",
-                  "mysql-pass" => "ph_pass",
-                  "mysql-db" => "ph_db",
-                  "mysql-platform" => "drupal7",
-                  "mysql-admin-user" => "root",
-                  "mysql-admin-pass" => "cleopatra",
-              ), ), ),
-
-              array ( "Logging" => array( "log" => array("log-message" => "Now lets install our database"), ), ),
-              array ( "DBInstall" => array( "install" => array(
-                  "parent-path" => "/var/www/drupal-hello/",
-                  "mysql-host" => "127.0.0.1",
-                  "mysql-user" => "ph_user",
-                  "mysql-pass" => "ph_pass",
-                  "mysql-db" => "ph_db",
-                  "mysql-platform" => "drupal7",
-                  "mysql-admin-user" => "root",
-                  "mysql-admin-pass" => "cleopatra",
-              ), ), ),
-
-              array ( "Logging" => array( "log" => array( "log-message" => "Now lets restart Apache so we are serving our new application version", ), ), ),
+              array ( "Logging" => array( "log" => array( "log-message" => "Now lets restart Apache so we are serving our Jenkins vhost", ), ), ),
               array ( "ApacheControl" => array( "restart" => array(
                   "guess" => true,
               ), ), ),
@@ -99,7 +57,7 @@ class AutoPilotConfigured extends AutoPilot {
             <<<'TEMPLATE'
            NameVirtualHost ****IP ADDRESS****:80
  <VirtualHost ****IP ADDRESS****:80>
-   ServerAdmin webmaster@localhost
+    ServerAdmin webmaster@localhost
  	ServerName ****SERVER NAME****
  	DocumentRoot ****WEB ROOT****src
  	<Directory ****WEB ROOT****src>
@@ -112,24 +70,15 @@ class AutoPilotConfigured extends AutoPilot {
    CustomLog /var/log/apache2/access.log combined
  </VirtualHost>
 
-# NameVirtualHost ****IP ADDRESS****:443
-# <VirtualHost ****IP ADDRESS****:443>
-# 	 ServerAdmin webmaster@localhost
-# 	 ServerName ****SERVER NAME****
-# 	 DocumentRoot ****WEB ROOT****src
-   # SSLEngine on
- 	 # SSLCertificateFile /etc/apache2/ssl/ssl.crt
-   # SSLCertificateKeyFile /etc/apache2/ssl/ssl.key
-   # SSLCertificateChainFile /etc/apache2/ssl/bundle.crt
-# 	 <Directory ****WEB ROOT****src>
-# 		 Options Indexes FollowSymLinks MultiViews
-#		AllowOverride All
-#		Order allow,deny
-#		allow from all
-#	</Directory>
-#  ErrorLog /var/log/apache2/error.log
-#  CustomLog /var/log/apache2/access.log combined
-#  </VirtualHost>
+ <VirtualHost ****IP ADDRESS****:80>
+    ServerAdmin webmaster@localhost
+ 	ServerName ****SERVER NAME****
+    ProxyPreserveHost On
+    ProxyPass / http://127.0.0.1:8080/
+    ProxyPassReverse / http://127.0.0.1:8080/
+    ServerName hostname.example.com
+</VirtualHost>
+
 TEMPLATE;
 
         return $template ;
