c5c972a011963cf8a58bd913f9dfeda8e38ea4ee
David Amanshia <dave@pharoah-tools.org.uk>
2014-09-05 15:13:33 +0100

    seperating commands that should be on two lines

--- capgemini-install.sh
+++ capgemini-install.sh
@@ -3,6 +3,7 @@ sudo apt-get install -y php5 git
 git clone http://git.pharaohtools.com/git/phpengine/cleopatra.git
 sudo php cleopatra/install-silent
 cd /var/www
-git clone https://github.com/phpengine/drupal-hello && cd drupal-hello
+git clone https://github.com/phpengine/drupal-hello
+cd drupal-hello
 sudo cleopatra auto x --af="build/config/cleopatra/cleofy/autopilots/custom/Phlagrant/cleofy-cm-cap-workstation.php"
 phlagrant up now
